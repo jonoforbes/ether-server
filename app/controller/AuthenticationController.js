ff0e14b370ea23093054cb86c7ad335ed499c4df
jonoforbes <jonoforbes@me.com>
2017-04-28 12:18:33 +0100

    Remove console lines

--- app/controller/AuthenticationController.js
+++ app/controller/AuthenticationController.js
@@ -20,7 +20,6 @@ let AuthenticationController = class AuthenticationController {
     constructor() {
     }
     register(req, res) {
-        console.log(req.body);
         var userId = '';
         var boxUserId = '';
         var userData = req.body;
@@ -37,7 +36,6 @@ let AuthenticationController = class AuthenticationController {
             ;
             boxUserId = data.id;
             req.body.boxUserId = boxUserId;
-            console.log(boxUserId);
             new UserSchema_1.User(req.body).save((error, user) => {
                 if (error) {
                     console.log(error);
@@ -67,9 +65,6 @@ let AuthenticationController = class AuthenticationController {
                 return;
             }
             let user = resp[0];
-            console.log('user', user);
-            console.log('req password', req.body.password);
-            console.log('user password', user.password);
             bcrypt.compare(req.body.password, user.password, (errC, isMatch) => {
                 if (!isMatch) {
                     res.status(400).send({ error: "Wrong Username/password combination" });
@@ -84,8 +79,6 @@ let AuthenticationController = class AuthenticationController {
                 let boxAccessToken = "";
                 box_api_1.boxAdminAPIClient._session.tokenManager.getTokensJWTGrant('user', user.boxUserId, function (err, accesstokenInfo) {
                     boxAccessToken = accesstokenInfo.accessToken;
-                    console.log('box access token', boxAccessToken);
-                    console.log('box user id', user.boxUserId);
                     res.send({
                         token: token,
                         login: user.login,
@@ -93,7 +86,8 @@ let AuthenticationController = class AuthenticationController {
                         lastName: user.lastName,
                         boxUserId: user.boxUserId,
                         boxAccessToken: boxAccessToken,
-                        userId: user._id
+                        userId: user._id,
+                        role: user.role
                     });
                 });
             });
