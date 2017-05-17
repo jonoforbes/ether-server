e7db200efda6fd17cab3d59213ca2bacf20a9720
jonoforbes <jonoforbes@me.com>
2017-05-17 14:38:52 +0100

    Leading slash fix

--- app/controller/ActivitiesController.js
+++ app/controller/ActivitiesController.js
@@ -13,6 +13,8 @@ const Methods_1 = require("controllers.ts/decorator/Methods");
 const Params_1 = require("controllers.ts/decorator/Params");
 const mongodb_1 = require("mongodb");
 const ActivitySchema_1 = require("../schema/ActivitySchema");
+const UserDataSchema_1 = require("../schema/UserDataSchema");
+const index_1 = require("../index");
 const auth_1 = require("../auth");
 var jwt = require("jsonwebtoken");
 const DATA_ACTIVITIES_ADD = "DATA_ACTIVITIES_ADD";
@@ -35,14 +37,144 @@ let ActivitiesController = class ActivitiesController {
             }
         });
     }
+    post(req, activityType) {
+        if (activityType == 'message') {
+            console.log('notification for a message');
+            UserDataSchema_1.UserData.find({ userId: req.userId }, (error, docs) => {
+                if (error) {
+                    console.log(error);
+                    return;
+                }
+                else {
+                    var activityHeader = `${docs[0].firstName} sent you a message`;
+                    var activityBody = {
+                        header: activityHeader,
+                        content: req.content,
+                        type: activityType,
+                        recipientId: req.recipientId,
+                        messageId: req._id,
+                        seen: false
+                    };
+                    console.log('pres save', activityBody);
+                    new ActivitySchema_1.Activity(activityBody).save((error, response) => {
+                        if (error) {
+                            console.log('error', error);
+                        }
+                        else {
+                            this.handleRt(req.recipientId, { type: DATA_ACTIVITIES_ADD, payload: { activity: response } });
+                            console.log('notification added', response);
+                        }
+                    });
+                    return;
+                }
+            });
+        }
+        if (activityType == 'addTask') {
+            console.log('notification for add task');
+            UserDataSchema_1.UserData.find({ userId: req.userId }, (error, docs) => {
+                if (error) {
+                    console.log(error);
+                    return;
+                }
+                else {
+                    var activityHeader = `${docs[0].firstName} sent you a task`;
+                    var activityBody = {
+                        header: activityHeader,
+                        content: req.content,
+                        type: activityType,
+                        recipientId: req.recipientId,
+                        taskId: req._id,
+                        seen: false
+                    };
+                    console.log('pre save', activityBody);
+                    new ActivitySchema_1.Activity(activityBody).save((error, response) => {
+                        if (error) {
+                            console.log('error', error);
+                            return;
+                        }
+                        else {
+                            this.handleRt(req.recipientId, { type: DATA_ACTIVITIES_ADD, payload: { activity: response } });
+                            console.log('notification added', response);
+                            return;
+                        }
+                    });
+                }
+            });
+        }
+        if (activityType == 'completeTask') {
+            console.log('notification for complete task');
+            UserDataSchema_1.UserData.find({ userId: req.userId }, (error, docs) => {
+                if (error) {
+                    console.log(error);
+                    return;
+                }
+                else {
+                    var activityHeader = `${docs[0].firstName} completed a task`;
+                    var activityBody = {
+                        header: activityHeader,
+                        content: req.content,
+                        type: activityType,
+                        recipientId: req.userId,
+                        taskId: req._id,
+                        seen: false
+                    };
+                    console.log('pre save', activityBody);
+                    new ActivitySchema_1.Activity(activityBody).save((errr, response) => {
+                        if (error) {
+                            console.log('error', error);
+                            return;
+                        }
+                        else {
+                            this.handleRt(req.userId, { type: DATA_ACTIVITIES_UPDATE, payload: { _id: response._id, activity: activityBody } });
+                            console.log('notification added', response);
+                            return;
+                        }
+                    });
+                }
+            });
+        }
+        else {
+            return;
+        }
+    }
+    delete(req, res) {
+        let userId = auth_1.handleAuth(req, res);
+        ActivitySchema_1.Activity.remove({ _id: new mongodb_1.ObjectID(req.params.id), userId: new mongodb_1.ObjectID(userId) }, (error) => {
+            if (error) {
+                res.send(error);
+                return;
+            }
+            else {
+                this.handleRt(userId, { type: DATA_ACTIVITIES_REMOVE, payload: { _id: req.params.id } });
+                res.sendStatus(200);
+            }
+        });
+    }
+    handleRt(userId, action) {
+        if (!index_1.clientIdsMap[userId]) {
+            return;
+        }
+        index_1.clientIdsMap[userId]
+            .forEach((clientInfo) => {
+            index_1.io.to('/#' + clientInfo.clientId).emit("UPDATE_REDUX", action);
+        });
+    }
 };
 __decorate([
     Methods_1.Get("/"),
     __param(0, Params_1.Req()),
     __param(1, Params_1.Res())
 ], ActivitiesController.prototype, "get", null);
+__decorate([
+    __param(0, Params_1.Req())
+], ActivitiesController.prototype, "post", null);
+__decorate([
+    Methods_1.Delete("/:id"),
+    __param(0, Params_1.Req()),
+    __param(1, Params_1.Res())
+], ActivitiesController.prototype, "delete", null);
 ActivitiesController = __decorate([
-    Controllers_1.JsonController("api/activities")
+    Controllers_1.JsonController("/api/activities")
 ], ActivitiesController);
 exports.ActivitiesController = ActivitiesController;
 //# sourceMappingURL=ActivitiesController.js.map
--- src/controller/ActivitiesController.ts
+++ src/controller/ActivitiesController.ts
@@ -14,7 +14,7 @@ const DATA_ACTIVITIES_REMOVE: string = "DATA_ACTIVITIES_REMOVE";
 const DATA_ACTIVITIES_UPDATE: string = "DATA_ACTIVITIES_UPDATE";
 const DATA_ACTIVITIES_ADD_ALL: string = "DATA_ACTIVITIES_ADD_ALL";
 
-@JsonController("api/activities")
+@JsonController("/api/activities")
 export class ActivitiesController {
     constructor() {
     }
@@ -33,6 +33,138 @@ export class ActivitiesController {
                 }
             });
     }
+
+    public post(@Req() req: any, activityType: String): void {
+        // console.log('notification body', req.body);
+        // console.log('notification', req);
+        if (activityType == 'message') {
+            console.log('notification for a message');
+            UserData.find({userId: req.userId}, (error: any, docs: any) => {
+                if (error) {
+                    console.log(error);
+                    return;
+                }
+                else {
+                    var activityHeader = `${docs[0].firstName} sent you a message`;
+                    var activityBody = {
+                        header: activityHeader,
+                        content: req.content,
+                        type: activityType,
+                        recipientId: req.recipientId,
+                        messageId: req._id,
+                        seen: false
+                    };
+                    console.log('pres save', activityBody);
+                    new Activity(activityBody).save((error: any, response: any) => {
+                        if (error) {
+                            console.log('error', error);
+                        }
+                        else {
+                            this.handleRt(req.recipientId, {type: DATA_ACTIVITIES_ADD, payload: {activity: response}});
+                            console.log('notification added', response);
+                        }
+                    })
+                    return;
+                }
+            });            
+        }
+        if (activityType == 'addTask') {
+            console.log('notification for add task');
+            UserData.find({userId: req.userId}, (error: any, docs: any) => {
+                if (error) {
+                    console.log(error);
+                    return;
+                }
+                else {
+                    var activityHeader = `${docs[0].firstName} sent you a task`;
+                    var activityBody = {
+                        header: activityHeader,
+                        content: req.content,
+                        type: activityType,
+                        recipientId: req.recipientId,
+                        taskId: req._id,
+                        seen: false
+                    };
+                    console.log('pre save', activityBody);
+                    new Activity(activityBody).save((error: any, response: any) => {
+                        if (error) {
+                            console.log('error', error);
+                            return;
+                        }
+                        else {
+                            this.handleRt(req.recipientId, {type: DATA_ACTIVITIES_ADD, payload: {activity: response}});
+                            console.log('notification added', response);
+                            return;
+                        }
+                    })
+                    
+                }
+            })
+        }
+        if (activityType == 'completeTask') {
+            console.log('notification for complete task');
+            UserData.find({userId: req.userId}, (error: any, docs: any) => {
+                if (error) {
+                    console.log(error);
+                    return;
+                }
+                else {
+                    var activityHeader = `${docs[0].firstName} completed a task`;
+                    var activityBody = {
+                        header: activityHeader,
+                        content: req.content,
+                        type: activityType,
+                        recipientId: req.userId,
+                        taskId: req._id,
+                        seen: false
+                    };
+                    console.log('pre save', activityBody);
+                    new Activity(activityBody).save((errr: any, response: any) => {
+                        if (error) {
+                            console.log('error', error);
+                            return;
+                        }
+                        else {
+                            this.handleRt(req.userId, {type: DATA_ACTIVITIES_UPDATE, payload: {_id: response._id, activity: activityBody}});
+                            console.log('notification added', response);
+                            return;
+                        }
+                    })
+                }
+            })
+        }
+        else {
+            return;
+        }
+
+    }
+
+    @Delete("/:id")
+    public delete(@Req() req: Request, @Res() res: Response): void {
+        let userId: string = handleAuth(req, res);
+        Activity.remove({_id: new ObjectID(req.params.id), userId: new ObjectID(userId)},
+            (error: any) => {
+                if (error) {
+                    res.send(error);
+                    return;
+                }
+                else {
+                    this.handleRt(userId, {type: DATA_ACTIVITIES_REMOVE, payload: {_id: req.params.id}});
+                    res.sendStatus(200);
+                }
+            });
+    }
+
+    private handleRt(userId: string, action: {type: string, payload: any}): void {
+
+        if(!clientIdsMap[userId]) {
+            return;
+        }
+        clientIdsMap[userId]
+            .forEach((clientInfo: {clientId: string, jwtToken: string}) => {
+                io.to('/#' + clientInfo.clientId).emit("UPDATE_REDUX", action);
+            });
+    }
     
 }
 
@@ -41,132 +173,4 @@ export class ActivitiesController {
 
 
 
-    // public post(@Req() req: any, notificationType: String): void {
-    //     // console.log('notification body', req.body);
-    //     // console.log('notification', req);
-    //     if (notificationType == 'message') {
-    //         console.log('notification for a message');
-    //         UserData.find({userId: req.userId}, (error: any, docs: any) => {
-    //             if (error) {
-    //                 console.log(error);
-    //                 return;
-    //             }
-    //             else {
-    //                 var notificationHeader = `${docs[0].firstName} sent you a message`;
-    //                 var notificationBody = {
-    //                     header: notificationHeader,
-    //                     content: req.content,
-    //                     type: notificationType,
-    //                     recipientId: req.recipientId,
-    //                     messageId: req._id,
-    //                     seen: false
-    //                 };
-    //                 console.log('pres save', notificationBody);
-    //                 new Notification(notificationBody).save((error: any, response: any) => {
-    //                     if (error) {
-    //                         console.log('error', error);
-    //                     }
-    //                     else {
-    //                         console.log('notification added', response);
-    //                     }
-    //                 })
-    //                 return;
-    //             }
-    //         });            
-    //     }
-    //     if (notificationType == 'addTask') {
-    //         console.log('notification for add task');
-    //         UserData.find({userId: req.userId}, (error: any, docs: any) => {
-    //             if (error) {
-    //                 console.log(error);
-    //                 return;
-    //             }
-    //             else {
-    //                 var notificationHeader = `${docs[0].firstName} sent you a task`;
-    //                 var notificationBody = {
-    //                     header: notificationHeader,
-    //                     content: req.content,
-    //                     type: notificationType,
-    //                     recipientId: req.recipientId,
-    //                     taskId: req._id,
-    //                     seen: false
-    //                 };
-    //                 console.log('pre save', notificationBody);
-    //                 new Notification(notificationBody).save((error: any, response: any) => {
-    //                     if (error) {
-    //                         console.log('error', error);
-    //                         return;
-    //                     }
-    //                     else {
-    //                         console.log('notification added', response);
-    //                         return;
-    //                     }
-    //                 })
-                    
-    //             }
-    //         })
-    //     }
-    //     if (notificationType == 'completeTask') {
-    //         console.log('notification for complete task');
-    //         UserData.find({userId: req.userId}, (error: any, docs: any) => {
-    //             if (error) {
-    //                 console.log(error);
-    //                 return;
-    //             }
-    //             else {
-    //                 var notificationHeader = `${docs[0].firstName} completed a task`;
-    //                 var notificationBody = {
-    //                     header: notificationHeader,
-    //                     content: req.content,
-    //                     type: notificationType,
-    //                     recipientId: req.userId,
-    //                     taskId: req._id,
-    //                     seen: false
-    //                 };
-    //                 console.log('pre save', notificationBody);
-    //                 new Notification(notificationBody).save((errr: any, response: any) => {
-    //                     if (error) {
-    //                         console.log('error', error);
-    //                         return;
-    //                     }
-    //                     else {
-    //                         this.handleRt(req.userId, {type: DATA_NOTIFICATIONS_UPDATE, payload: {_id: response._id, notification: notificationBody}});
-    //                         console.log('notification added', response);
-    //                         return;
-    //                     }
-    //                 })
-    //             }
-    //         })
-    //     }
-    //     else {
-    //         return;
-    //     }
-
-    // }
-
-    // @Delete("/:id")
-    // public delete(@Req() req: Request, @Res() res: Response): void {
-    //     let userId: string = handleAuth(req, res);
-    //     Notification.remove({_id: new ObjectID(req.params.id), userId: new ObjectID(userId)},
-    //         (error: any) => {
-    //             if (error) {
-    //                 res.send(error);
-    //                 return;
-    //             }
-    //             else {
-    //                 this.handleRt(userId, {type: DATA_NOTIFICATIONS_REMOVE, payload: {_id: req.params.id}});
-    //                 res.sendStatus(200);
-    //             }
-    //         });
-    // }
-
-    // private handleRt(userId: string, action: {type: string, payload: any}): void {
-
-    //     if(!clientIdsMap[userId]) {
-    //         return;
-    //     }
-    //     clientIdsMap[userId]
-    //         .forEach((clientInfo: {clientId: string, jwtToken: string}) => {
-    //             io.to('/#' + clientInfo.clientId).emit("UPDATE_REDUX", action);
-    //         });
-    // }
+    
